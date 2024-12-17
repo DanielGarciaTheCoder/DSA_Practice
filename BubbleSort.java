977240f24e8ae06f1cecc926a4fb815a726f12a9
ProgramDan <106444964+DanielGarciaTheCoder@users.noreply.github.com>
2024-12-17 11:32:09 -0600

    FIles added

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,8 +1,8 @@
 public class BubbleSort {
-    
+
     public static void bubble_sort(int[] array) {
-        for (int i=0; i<array.length; i++) {
-            for(int j=0; j<array.length-1-i; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
@@ -11,16 +11,16 @@ public class BubbleSort {
             }
         }
     }
-    
+
     public static void main(String[] args) {
-        int[] array = {1, 22, 4, 728, 13, 53};
+        int[] array = { 1, 22, 4, 728, 13, 53 };
 
         bubble_sort(array);
 
-        for (int i: array) {
+        for (int i : array) {
             System.out.print(i + " ");
         }
-        
+
         System.out.println();
     }
 }
---
+++ NewQueue.java
@@ -0,0 +1,15 @@
+public class NewQueue {
+    int length;
+    int value;
+    NewQueue next;
+
+    public NewQueue(int value, NewQueue next) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
